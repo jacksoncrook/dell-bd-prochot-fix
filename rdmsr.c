138184e2bcb9b89cb4dc59d1bf4846061d647f43
Jackson Crook <jacksoncrook01@gmail.com>
2026-03-25 10:57:46 -0600

    Removed commandline flag functionality from rdmsr.c

--- rdmsr.c
+++ rdmsr.c
@@ -17,36 +17,16 @@
  * Utility to read an MSR.
  */
 
-#include <errno.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <ctype.h>
+#include <errno.h>//
+#include <stdio.h>//
+#include <fcntl.h>//open
+//#include <unistd.h>
+#include <stdlib.h>//
+#include <sys/types.h>//
+#include <dirent.h>//
 
-#include "version.h"
-
-static const struct option long_options[] = {
-	{"help",		0, 0, 'h'},
-	{"version",		0, 0, 'V'},
-	{"bitfield",		1, 0, 'f'},
-	{0, 0, 0, 0}
-};
-static const char short_options[] = "hV:f:";
 
 const char *program;
-
-void usage(void)
-{
-	fprintf(stderr,
-		"Usage: %s [options] regno\n"
-		"  --help         -h  Print this help\n"
-		"  --version      -V  Print current version\n"
-		"  --bitfield h:l -f  Output bits [h:l] only\n", program);
-}
 
 void rdmsr_on_cpu(uint32_t reg, int cpu);
 
@@ -75,45 +55,9 @@ unsigned int highbit = 63, lowbit = 0;
 
 int main(int argc, char *argv[])
 {
-	uint32_t reg;
-	int c;
-	int cpu = 0;
-	unsigned long arg;
-	char *endarg;
+	uint32_t reg = 0x1FC;
 
 	program = argv[0];
-
-	while ((c =
-		getopt_long(argc, argv, short_options, long_options,
-			    NULL)) != -1) {
-		switch (c) {
-		case 'h':
-			usage();
-			exit(0);
-		case 'V':
-			fprintf(stderr, "%s: version %s\n", program,
-				VERSION_STRING);
-			exit(0);
-		case 'f':
-			if (sscanf(optarg, "%u:%u", &highbit, &lowbit) != 2 ||
-			    highbit > 63 || lowbit > highbit) {
-				usage();
-				exit(127);
-			}
-			break;
-		default:
-			usage();
-			exit(127);
-		}
-	}
-
-	if (optind != argc - 1) {
-		/* Should have exactly one argument */
-		usage();
-		exit(127);
-	}
-
-	reg = strtoul(argv[optind], NULL, 0);
 
 	rdmsr_on_all_cpus(reg);
 	exit(0);
@@ -124,9 +68,7 @@ void rdmsr_on_cpu(uint32_t reg, int cpu)
 	uint64_t data;
 	int fd;
 	char *pat;
-	int width;
 	char msr_file_name[64];
-	unsigned int bits;
 
 	sprintf(msr_file_name, "/dev/cpu/%d/msr", cpu);
 	fd = open(msr_file_name, O_RDONLY);
@@ -156,14 +98,6 @@ void rdmsr_on_cpu(uint32_t reg, int cpu)
 	}
 
 	close(fd);
-
-	bits = highbit - lowbit + 1;
-	if (bits < 64) {
-		/* Show only part of register */
-		data >>= lowbit;
-		data &= (1ULL << bits) - 1;
-	}
-
 
 	printf("CPU %d: ", cpu);
 	printf("%1llx\n", data);
